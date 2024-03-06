c1e25b7e7a2bbc5e8e013b4ea42cbaf48459ee62
K1mmyn <125098192+K1mmyn@users.noreply.github.com>
2024-03-06 13:22:52 +1300

    first commit

---
+++ 11-fs-async.js
@@ -0,0 +1,31 @@
+const { readFile, writeFile } = require("fs")
+
+console.log("start")
+readFile("./content/first.txt", "utf-8", (err, result) => {
+   if (err) {
+      console.log(err)
+      return null
+   }
+   const first = result
+   readFile("./content/second.txt", "utf-8", (err, result) => {
+      if (err) {
+         console.log(err)
+         return
+      }
+      const second = result
+      writeFile(
+         "./content/result-asynce.txt",
+         `Here is the result: ${first}, ${second}`,
+         (err, result) => {
+            if (err) {
+               console.log(err)
+               return
+            }
+            console.log("done")
+         }
+      )
+   })
+})
+
+console.log("starting next task")
+
---
+++ 4-names.js
@@ -0,0 +1,9 @@
+//* local
+const secret = "SUPER SECRET";
+
+//* Share
+
+const john = "john";
+const peter = "peter";
+
+module.exports = { john, peter }
